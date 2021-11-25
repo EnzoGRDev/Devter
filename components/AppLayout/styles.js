e5bf8e3f2d7d130fdd58dcdb40537fceac0ad9d2
EnzoGRDev <enzo.g.rz.18@gmail.com>
2021-11-25 15:36:34 -0300

    first commit

---
+++ components/AppLayout/styles.js
@@ -0,0 +1,42 @@
+import css from 'styled-jsx/css'
+import { breakpoints, colors, fonts } from "../../styles/theme"
+
+
+export const globalStyles = css.global`
+html, body{
+  background-image: 
+  radial-gradient(${colors.primary}44 1px, transparent 1px),
+  radial-gradient(${colors.primary}44 1px, transparent 1px);
+  background-position: 0 0, 25px 25px;
+  background-size: 50px 50px;
+  padding: 0;
+  margin: 0;
+  font-family: ${fonts.base}; 
+}
+*{
+  box-sizing: border-box; 
+}
+`
+
+export default css`
+div{
+  display: grid;
+  height: 100vh;
+  place-items: center;
+}
+main{
+  background: transparent;
+  border-radius: 10px;
+  box-shadow: 0 10px 25px rgba(0, 0, 0, .3);
+  height: 100%;
+  width: 100%;
+}
+
+@media (min-width: ${breakpoints.mobile}){
+  main{
+    background: #fff;
+    height: 90vh;
+    width: ${breakpoints.mobile};
+  }
+}
+`
---
+++ firebaseConfig/client.js
@@ -0,0 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp , getApps, getApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { GithubAuthProvider, signInWithPopup, getAuth, onAuthStateChanged, getModularInstance  } from "firebase/auth"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBEKN-Ffpz3i_fCEF-kwiNPUfmpoiMuRuE",
+  authDomain: "devter-d92a4.firebaseapp.com",
+  projectId: "devter-d92a4",
+  storageBucket: "devter-d92a4.appspot.com",
+  messagingSenderId: "78211422390",
+  appId: "1:78211422390:web:b059f1483f54ef06cb2e17",
+  measurementId: "G-10ERWZW90J"
+};
+
+// Initialize Firebase
+getApps().length === 0 && initializeApp(firebaseConfig);
+const app = getApp()
+const authApp = getAuth(app)
+
+function mapUserFromFirebaseAuthTo ({_tokenResponse, reloadUserInfo}){
+  console.log(_tokenResponse, reloadUserInfo)
+
+  const { photoUrl, screenName } = _tokenResponse ? _tokenResponse : reloadUserInfo
+  return { photo: photoUrl, username: screenName }
+}
+
+export function handleOnAuthStateChanged (onChange){
+  return onAuthStateChanged(authApp, (user)=>{
+    const normalizedUser = mapUserFromFirebaseAuthTo(user)
+    onChange(normalizedUser)
+  } )
+} 
+
+export default function loginWithGithub(){
+  const githubProvider = new GithubAuthProvider()
+  return signInWithPopup( authApp, githubProvider)
+    .then(mapUserFromFirebaseAuthTo)
+    .catch(error  => console.error(error))}
